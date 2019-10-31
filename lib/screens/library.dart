d77f34ee5068a2786b0ee31ec4f20ec3dbec18ac
Nikhil Singh <Singh.nikhil999@gmail.com>
2019-11-01 02:28:58 +0530

    added tab bar for library and podcasts

--- lib/screens/library.dart
+++ lib/screens/library.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class LibraryTab extends StatefulWidget {
@@ -7,13 +8,71 @@ class LibraryTab extends StatefulWidget {
   _LibraryTabState createState() => _LibraryTabState();
 }
 
-class _LibraryTabState extends State<LibraryTab> {
+class _LibraryTabState extends State<LibraryTab> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("lib Screen"),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        flexibleSpace: SafeArea(
+          child: TabBar(
+            unselectedLabelColor: Colors.grey,
+            dragStartBehavior: DragStartBehavior.start,
+            controller: _tabController,
+            tabs: <Widget>[
+              Tab(
+                child: Text("Music",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900
+                  ),),
+              ),
+              Tab(
+                child: Text("Podcasts",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900
+                  ),),
+              ),
+            ],
+          ),
+        ),
       ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Container(
+            child: Text("Music",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.w900
+            ),),
+          ),
+
+
+          // tab 2
+          Container(
+            child: Text("Podcasts",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900
+              ),),
+          )
+        ],
+
+      ),
+
     );
   }
 }
