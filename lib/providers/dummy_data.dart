a466b8c7436a2923497b343512e9b7665817c5b7
Nikhil Singh <Singh.nikhil999@gmail.com>
2019-11-02 22:23:26 +0530

    added dummy data

---
+++ lib/providers/dummy_data.dart
@@ -0,0 +1,51 @@
+import '../models/albumData.dart';
+import 'package:flutter/material.dart';
+
+var dummyAlbums = [
+  AlbumData(
+    id: 'c1',
+    title: 'This is Jon Bellion',
+    imageUrl: "https://i.scdn.co/image/cb406b760562e264ea5945c5d7352341232cf697",
+  ),
+  AlbumData(
+    id: 'c2',
+    title: 'On Repeat',
+    imageUrl: "https://daily-mix.scdn.co/covers/on_repeat/PZN_On_Repeat_DEFAULT-en.jpg",
+  ),
+
+  AlbumData(
+    id: 'c3',
+    title: 'Rewind - The Sound of 2018',
+    imageUrl: "https://pl.scdn.co/images/pl/default/a9f8cd2e5511ac57ad06bc1acec4a7bc145f0f54",
+  ),
+
+  AlbumData(
+    id: 'c4',
+    title: 'This is Twenty One Pilots',
+    imageUrl: "https://pl.scdn.co/images/pl/default/62e6e7d73f621a460cfd0d37a3c20f327c7c2dc4",
+  ),
+
+  AlbumData(
+    id: 'c5',
+    title: 'This is Twenty One Pilots',
+    imageUrl: "https://www.music-bazaar.com/album-images/vol32/1957/1957337/3836078-big/Today-s-Top-Hits-Spotify-20-05-2019-cover.jpg",
+  ),
+
+  AlbumData(
+    id: 'c6',
+    title: 'This is NF',
+    imageUrl: "https://i.scdn.co/image/d0968650a052c15533f29e372a37ee0f2f0e55ff",
+  ),
+
+  AlbumData(
+    id: 'c7',
+    title: 'This is Shankar Ehsan Loy',
+    imageUrl: "https://pl.scdn.co/images/pl/default/6ae00d67d6452510b82ef2b632cea46bb41c6a6e",
+  ),
+
+  AlbumData(
+    id: 'c8',
+    title: 'Best Of Camila Cabello',
+    imageUrl: "https://www.billboard.com/files/styles/article_main_image/public/media/camila-cabello-london-june-2018-billboard-1548.jpg",
+  ),
+];
