2fd10cd9f2335af9d6157595c2c2d9f70c22cba6
Nikhil Singh <Singh.nikhil999@gmail.com>
2019-11-03 22:42:04 +0530

    added search field and other buttons for library

--- lib/screens/library.dart
+++ lib/screens/library.dart
@@ -8,7 +8,8 @@ class LibraryTab extends StatefulWidget {
   _LibraryTabState createState() => _LibraryTabState();
 }
 
-class _LibraryTabState extends State<LibraryTab> with SingleTickerProviderStateMixin {
+class _LibraryTabState extends State<LibraryTab>
+    with SingleTickerProviderStateMixin {
   TabController _tabController;
 
   @override
@@ -16,32 +17,38 @@ class _LibraryTabState extends State<LibraryTab> with SingleTickerProviderStateM
     _tabController = TabController(length: 2, vsync: this);
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
+        backgroundColor: Colors.black,
+        bottomOpacity: 0.0,
         flexibleSpace: SafeArea(
           child: TabBar(
-            unselectedLabelColor: Colors.grey,
+            unselectedLabelColor: Colors.white,
             dragStartBehavior: DragStartBehavior.start,
             controller: _tabController,
             tabs: <Widget>[
               Tab(
-                child: Text("Music",
+                child: Text(
+                  "Music",
+                  textAlign: TextAlign.left,
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 20,
-                      fontWeight: FontWeight.w900
-                  ),),
+                      fontWeight: FontWeight.w900),
+                ),
               ),
               Tab(
-                child: Text("Podcasts",
+                child: Text(
+                  "Podcasts",
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 20,
-                      fontWeight: FontWeight.w900
-                  ),),
+                      fontWeight: FontWeight.w900),
+                ),
               ),
             ],
           ),
@@ -51,28 +58,114 @@ class _LibraryTabState extends State<LibraryTab> with SingleTickerProviderStateM
         controller: _tabController,
         children: <Widget>[
           Container(
-            child: Text("Music",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.w900
-            ),),
-          ),
+              child: ListView(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(left: 15, right: 5, top: 10),
+                child: Row(
+                  children: <Widget>[
+                    FlatButton(
+                      child: Text(
+                        "Playlists",
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: null,
+                    ),
+                    FlatButton(
+                      child: Text(
+                        "Artists",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: null,
+                    ),
+                    FlatButton(
+                      child: Text(
+                        "Albums",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: null,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                  height: 30,
+                  margin: EdgeInsets.all(10),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
 
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.white30,
+                        ),
+                        width: 270,
+                        child: TextField(
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.search,
+                              size: 24,
+                              color: Colors.white,
+                            ),
+                            hintText: "Find in playlists",
+                            hintStyle: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 14,
+                                color: Colors.white),
+                            alignLabelWithHint: true,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 50,
+                        margin: EdgeInsets.only(left: 20),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.white30,
+                        ),
+                        child: RaisedButton(
+                          child: Text(
+                            "Albums",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white,
+                            ),
+                          ),
+                          onPressed: null,
+                        ),
+                      )
+                    ],
+                  )),
+            ],
+          )),
 
           // tab 2
           Container(
-            child: Text("Podcasts",
+            child: Text(
+              "Podcasts",
               style: TextStyle(
                   color: Colors.white,
                   fontSize: 20,
-                  fontWeight: FontWeight.w900
-              ),),
-          )
+                  fontWeight: FontWeight.w900),
+            ),
+          ),
         ],
-
       ),
-
     );
   }
 }
