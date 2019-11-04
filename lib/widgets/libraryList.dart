5e0634db88a47583f5b10a5aab78a9fb4f87fd06
Nikhil Singh <Singh.nikhil999@gmail.com>
2019-11-05 02:02:57 +0530

    added library card widget

--- lib/widgets/libraryList.dart
+++ lib/widgets/libraryList.dart
@@ -1,13 +1,62 @@
 import 'package:flutter/material.dart';
 
 class LibraryCard extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String subTitle;
+
+  LibraryCard(this.imageUrl, this.title, this.subTitle);
+
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
     return Card(
-      child: Row(
-        children: <Widget>[
-          
-        ],
+      child: Container(
+        padding: EdgeInsets.all(10),
+        color: Colors.black,
+//        height: 100,
+        width: deviceSize.width,
+        child: Row(
+          children: <Widget>[
+            Container(
+              color: Colors.white30,
+              height: 70,
+              width: 70,
+              child: Image.network(
+                imageUrl,
+              ),
+            ),
+            SizedBox(
+              width: 25,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Container(
+                  child: Text(
+                    subTitle,
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w700),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
