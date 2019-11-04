4d9e8092fd49762ff869ccc639a7ffbafafb7053
Nikhil Singh <Singh.nikhil999@gmail.com>
2019-11-05 02:02:04 +0530

    added model for library data

---
+++ lib/models/libraryData.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class LibraryData{
+  final String imageUrl;
+  final String title;
+  final String subTitle;
+
+  LibraryData({
+    @required this.imageUrl,
+    @required this.title,
+    @required this.subTitle
+  });
+}
