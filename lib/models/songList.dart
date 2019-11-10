5965172772b79425f37299759d01f9c098f92ead
Nikhil Singh <Singh.nikhil999@gmail.com>
2019-11-10 22:44:51 +0530

    created song list model

---
+++ lib/models/songList.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SongList {
+  final String title;
+  final String subTitle;
+  final String albumName;
+
+  SongList({
+    @required this.title,
+    @required this.subTitle,
+    @required this.albumName
+  });
+
+
+}
